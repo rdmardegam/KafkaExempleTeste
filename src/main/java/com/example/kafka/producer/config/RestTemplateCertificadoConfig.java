6600b45be4323ab2766c2d2d2e6fa02313a9de91
rdmardegam <rdmardegam@hotmail.com>
2021-06-02 09:06:51 -0300

    TESTE COM MASTERCARD

---
+++ src/main/java/com/example/kafka/producer/config/RestTemplateCertificadoConfig.java
@@ -0,0 +1,180 @@
+package com.example.kafka.producer.config;
+
+import java.util.Collections;
+
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateCertificadoConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate template = new RestTemplate();
+		
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+		connectionManager.setMaxTotal(100);
+		connectionManager.setDefaultMaxPerRoute(6);
+		template.setRequestFactory(new HttpComponentsClientHttpRequestFactory(HttpClients.custom().setConnectionManager(connectionManager).build()));
+		
+		template.setInterceptors( Collections.singletonList(new RequestResponseLoggingInterceptor()));
+		
+		System.out.println("RESTTEMPLATE INIT");
+		
+		return template;
+	}
+	
+	
+	
+//	@Bean
+//	public RestTemplate restTemplate() throws Exception {
+//	    KeyStore clientStore = KeyStore.getInstance("PKCS12");
+//	    clientStore.load(new FileInputStream("/path/to/certfile"), "certpassword".toCharArray());
+
+//		KeyStore clientStore = KeyStore.getInstance(new File("./keyalias-sandbox.p12"),"keystorepassword1".toCharArray());
+//
+//	    SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+//	    sslContextBuilder.useProtocol("TLS");
+//	    sslContextBuilder.loadKeyMaterial(clientStore, "certpassword".toCharArray());
+//	    //sslContextBuilder.loadTrustMaterial(new TrustSelfSignedStrategy());
+//
+//	    SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build());
+//	    CloseableHttpClient httpClient = HttpClients.custom()
+//	            .setSSLSocketFactory(sslConnectionSocketFactory)
+//	            .build();
+//	    
+//	    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//	    requestFactory.setConnectTimeout(10000); // 10 seconds
+//	    requestFactory.setReadTimeout(10000); // 10 seconds
+//	    
+//	    return new RestTemplate(requestFactory);
+
+//		
+//		System.out.println("CARREGANDO CERTIFICADO");
+//		
+//		KeyStore clientStore = KeyStore.getInstance("PKCS12");
+//	    clientStore.load(new FileInputStream("./keyalias-sandbox.p12"), "keystorepassword1".toCharArray());
+//
+//	    SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+//	    sslContextBuilder.useProtocol("TLS");
+//	    sslContextBuilder.loadKeyMaterial(clientStore, "keystorepassword1".toCharArray());
+//	    //sslContextBuilder.loadTrustMaterial(clientStore);
+//
+//	    SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build());
+//	    CloseableHttpClient httpClient = HttpClients.custom()
+//	            .setSSLSocketFactory(sslConnectionSocketFactory)
+//	            .build();
+//	    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//	    return new RestTemplate(requestFactory);
+
+//	}
+
+//	@Bean
+//	public HttpClient httpClient(SSLContext sslContext) {
+//		return HttpClients.custom().setMaxConnPerRoute(30).setMaxConnTotal(60).setSSLContext(sslContext).build();
+//	}
+//
+//	@Bean
+//	public ClientHttpRequestFactory clientHttpRequestFactory(HttpClient httpClient) {
+//		return new HttpComponentsClientHttpRequestFactory(httpClient);
+//	}
+//
+//	@Bean
+//	public RestTemplate restOperations(ClientHttpRequestFactory clientHttpRequestFactory) throws Exception {
+//		System.out.println("CARREGANDO RESTTEMPLATE");
+//		return new RestTemplate(clientHttpRequestFactory);
+//	}
+//
+//	@Autowired
+//	private ResourceLoader resourceLoader;
+//
+//	@Bean
+//	public SSLContext sslContext() throws Exception {
+//		
+//		
+//		// load the keystore file as input stream
+//		InputStream keystoreStream = resourceLoader.getResource("./keyalias-sandbox.p12").getInputStream();
+//
+//		KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+//		// get instance of keystore depends on type of store here we have //pkcs12
+//		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+//		try {
+//			keyStore.load(keystoreStream, "keystorepassword1".toCharArray());
+//		} finally { 
+//			keystoreStream.close();
+//		}
+//		keyManagerFactory.init(keyStore, "keystorepassword1".toCharArray());
+//		KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
+//		// load trust store file as input stream
+//		KeyStore trustStore = KeyStore.getInstance("PKCS12");
+//		InputStream truststoreSteam = resourceLoader.getResource("./keyalias-sandbox.p12").getInputStream();
+//
+//		try {
+//			// load trust store
+//			trustStore.load(truststoreSteam, "keystorepassword1".toCharArray());
+//		} finally {
+//			truststoreSteam.close();
+//		}
+//		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+//		tmf.init(trustStore);
+//
+//		SSLContext sslcontext = SSLContext.getInstance("TLS");
+//
+//		// initialize ssl context with both key manager and trust manager
+//		sslcontext.init(keyManagers, tmf.getTrustManagers(), null);
+//		 
+//		System.out.println("CARREGANDO SSLW");
+//		
+//		// return sslContext
+//		return sslcontext; 
+//	}
+
+	
+	
+	
+	
+
+//	private String getHeaderOath() throws Exception {
+//		PrivateKey signingKey = AuthenticationUtils.loadSigningKey("./keyalias-sandbox.p12", "keyalias","keystorepassword1");
+//		HttpHeaders header = new HttpHeaders();
+//		URI uri = new URI("https://api.mastercard.com/service");
+//		
+//		HttpRequest request = new HttpRequest() {
+//			@Override
+//			public HttpMethod getMethod() {
+//				return HttpMethod.POST;
+//			}
+//
+//			@Override
+//			public String getMethodValue() {
+//				return getMethod().toString();
+//			}
+//
+//			@Override
+//			public URI getURI() {
+//				return uri;
+//			}
+//
+//			@Override
+//			public HttpHeaders getHeaders() {
+//				return header;
+//			}
+//		};
+//
+//		SpringHttpRequestSigner instanceUnderTest = new SpringHttpRequestSigner(
+//				"6ynVPNsWkA59SZXvieIRJ7tbapzylu9jxaq3te4re9dca799!5f9bae87b9fa4b6faee7cfacfdce5e9f0000000000000000",
+//				signingKey);
+//		instanceUnderTest.sign(request, null);
+//
+//		System.out.println(header.getFirst(HttpHeaders.AUTHORIZATION));
+//
+//		return header.getFirst(HttpHeaders.AUTHORIZATION);
+//
+//	}
+
+	
+}
---
+++ src/main/java/com/example/kafka/producer/controller/CertificadoController.java
@@ -0,0 +1,120 @@
+package com.example.kafka.producer.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.PrivateKey;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.mastercard.developer.signers.SpringHttpRequestSigner;
+import com.mastercard.developer.utils.AuthenticationUtils;
+
+@RestController
+public class CertificadoController {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("findStatus")
+	public String findStatus () throws Exception {
+		
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//	    String url = "https://sandbox.api.mastercard.com/mdes/csapi/v2/systemstatus";
+//	    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+//	  //  String result = restTemplate.getForObject(uri, String.class);
+//	    //System.out.println("Result " + result);
+//		
+//	    
+//	    
+//	    PrivateKey signingKey = AuthenticationUtils.loadSigningKey(
+//                "./keyalias-sandbox.p12", 
+//                "keyalias", 
+//                "keystorepassword1");
+//		
+//		
+//		String consumerKey = "6ynVPNsWkA59SZXvieIRJ7tbapzylu9jxaq3te4re9dca799!5f9bae87b9fa4b6faee7cfacfdce5e9f0000000000000000";
+//		URI uri = URI.create(url);
+//		String method = "GET";
+//		String payload = "Hello world!";
+//		Charset charset = StandardCharsets.UTF_8;
+//		String authHeader = OAuth.getAuthorizationHeader(uri, method, payload, charset, consumerKey, signingKey);
+//		
+//		System.out.println(authHeader);
+//	    //----
+//		
+//		httpHeaders.add(OAuth.AUTHORIZATION_HEADER_NAME, authHeader);
+//		
+//		
+//		HttpEntity entity = new HttpEntity(httpHeaders);
+//
+//		ResponseEntity<String> response = restTemplate.exchange(
+//		    url, HttpMethod.GET, entity, String.class);
+//		
+//		System.out.println(response.getStatusCode());
+//		System.out.println(response.getBody());
+//		
+//		return response.getBody();
+		
+	  PrivateKey signingKey = AuthenticationUtils.loadSigningKey(
+      "./keyalias-sandbox.p12", 
+      "keyalias", 
+      "keystorepassword1");
+		
+	    HttpHeaders header = new HttpHeaders();
+	    
+	    HttpRequest request = new HttpRequest() {
+			@Override
+			public HttpMethod getMethod(){
+				return HttpMethod.POST;
+			}
+			@Override
+			public String getMethodValue(){
+				return getMethod().toString();
+			}
+			@Override
+			public URI getURI(){
+				try {
+					return new URI("https://api.mastercard.com/service");
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}
+			@Override
+			public HttpHeaders getHeaders(){
+				return header;
+			}
+		};
+		
+		SpringHttpRequestSigner instanceUnderTest = new SpringHttpRequestSigner("6ynVPNsWkA59SZXvieIRJ7tbapzylu9jxaq3te4re9dca799!5f9bae87b9fa4b6faee7cfacfdce5e9f0000000000000000", signingKey);
+		instanceUnderTest.sign(request, null);
+		
+		System.out.println(header.getFirst(HttpHeaders.AUTHORIZATION));
+		
+		return header.toString();
+	}
+	
+	@GetMapping("findStatus2")
+	public String findStatus2 () throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+		HttpEntity entity = new HttpEntity(headers);
+		
+		
+		String url = "https://sandbox.api.mastercard.com/mdes/csapi/v2/systemstatus";	
+		ResponseEntity<String> response = restTemplate.exchange(
+	    url, HttpMethod.GET, entity, String.class);
+		
+		return response.getBody();
+	}
+}
