464a7eaec8934c68f67871bdb48a2c7a0b4e2624
rdmardegam <rdmardegam@hotmail.com>
2021-09-13 01:39:56 -0300

    Consumer com retry

--- src/main/java/com/example/kafka/producer/listener/MasterCardConsumer.java
+++ src/main/java/com/example/kafka/producer/listener/MasterCardConsumer.java
@@ -2,10 +2,12 @@ package com.example.kafka.producer.listener;
 
 import java.net.ConnectException;
 import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +18,23 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 
+import com.example.kafka.producer.enums.TopicEnum;
 import com.example.kafka.producer.exception.TechnicalException;
 import com.example.kafka.producer.model.MasterCardDTO;
 import com.example.kafka.producer.service.CardService;
 import com.example.kafka.producer.utils.LogSplunk;
 import com.example.kafka.producer.utils.Splunk;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
@@ -44,7 +53,10 @@ public class MasterCardConsumer {
 	ObjectMapper mapper;
 
 	@Autowired
-	private KafkaTemplate<String, Map<String,String>> kafkaTemplate;
+	private KafkaTemplate<String, Map<String,Object>> kafkaTemplate;
+	
+	/*@Autowired
+	private KafkaTemplate<String, String> kafkaTemplateRetry;*/
 	
 	@Autowired
     private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
@@ -65,22 +77,81 @@ public class MasterCardConsumer {
 	}
 	
 	@KafkaListener(id = "MASTER_CARD_TOKEN_ACTIVATION", topics = "MASTER_CARD_TOKEN_ACTIVATION", groupId = "consumer_group1")
-	public void messageListener(ConsumerRecord<String, ?> consumerRecord, /*@Payload String payload,*/ Acknowledgment ack)throws Exception {
+	public void messageListener(ConsumerRecord<String, String> consumerRecord, /*@Payload String payload,*/ Acknowledgment ack) throws Exception {
+		System.out.println("\n\n");
+		System.out.println("+++++++++++++++++++++++  CONSUMINDO ORIGINAL");
+		System.out.println("\n\n");
+
+		processMsg(consumerRecord, ack);
+	}
+
+	//@Header("dataHoraProximaTentativa") long dataHoraProximaTentativa
+	@KafkaListener(id = "MASTER_CARD_TOKEN_RETRY1", topics = "MASTER_CARD_TOKEN_ACTIVATION_PRIMEIRO_RETRY", groupId = "consumer_group1")
+	public void messageListenerRetry(ConsumerRecord<String, String> consumerRecord, Acknowledgment ack ,  @Header(KafkaHeaders.RECEIVED_TIMESTAMP)  long ts ) throws Exception {
+		System.out.println("\n\n");
+		System.out.println("CONSUMINDO ************** RETRY 1");
+		
+		if(System.currentTimeMillis() >= ts + (1000 * 10) ) {
+			System.out.println(" PROCESSANDO O RETRY 1 AGORA ");
+			processMsg(consumerRecord, ack);	
+		} else {
+			System.out.println("Falta RETRY 1: " + (ts + (1000 * 10) - System.currentTimeMillis() ));
+			ack.nack( (1000 * 5));
+		}
+	}
+	
+	@KafkaListener(id = "MASTER_CARD_TOKEN_RETRY2", topics = "MASTER_CARD_TOKEN_ACTIVATION_SEGUNDO_RETRY", groupId = "consumer_group1")
+	public void messageListenerRetry2(ConsumerRecord<String, String> consumerRecord, Acknowledgment ack, @org.springframework.messaging.handler.annotation.Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts ) throws Exception {
+		System.out.println("\n\n");
+		System.out.println("CONSUMINDO ************** RETRY 2");
+
+		//LogSplunk.info(Splunk.builder().key("RETRY 2").customMessage("RETRY 2").build());
+		
+		if(System.currentTimeMillis() >= ts + (1000 * 40)) {
+			System.out.println(" PROCESSANDO O RETRY 2 AGORA ");
+			processMsg(consumerRecord, ack);
+		} else {
+			System.out.println("Falta RETRY 2 : " + (ts + (1000 * 40) - System.currentTimeMillis() ));
+			//ack.nack(ts + (1000 * 10) - System.currentTimeMillis());
+			ack.nack((1000 * 10));
+		}
+	}
+	
+	
+//	private long getNextExecutionByHeader(Headers headers, String headerName) {
+//		long value = 0L;
+//		
+//		try {
+//			for (Header header : headers) {
+//	            if (header.key().equals(headerName)) {
+//	               String s = new String(header.value(), StandardCharsets.UTF_8);
+//	               System.out.println(s);
+//	               value =  Long.valueOf(s);
+//	               break;
+//	            }
+//			 }
+//		} catch (Exception e) {
+//			value = 0L;
+//		}
+//
+//		
+//		return value;
+//	}
+	
+	private void processMsg(ConsumerRecord<String, String> consumerRecord,  Acknowledgment ack) throws Exception {
 		long startTime = System.currentTimeMillis();
 		
-		// Send log inicio de processamento msg
-		LogSplunk.info(Splunk.builder().key("token.ativacao")
-				 .customMessage("Iniciando ativacao token")
-				 .payload(consumerRecord.toString()).build());
-
-		MasterCardDTO masterCard = null;
+		System.out.println("*** TOPIC NAME ***");
+		System.out.println(consumerRecord.topic());
+		System.out.println("***  ***");
 		
-		String payload = (String) consumerRecord.value().toString();
+		MasterCardDTO masterCard = null;
+		String payload = consumerRecord.value();
 		
 		try {
 			// Tenta transformar o payload em Objeto
-			masterCard = mapper.readValue(payload, MasterCardDTO.class);
-
+			masterCard = mapper.treeToValue(mapper.readTree(payload).get("payload"), MasterCardDTO.class);
+			
 			LogSplunk.info(Splunk.builder().key("token.ativacao2")
 					 	   .customMessage("Iniciando ativacao token")
 					       .payload(masterCard).build());
@@ -103,49 +174,129 @@ public class MasterCardConsumer {
 						.customMessage("Token ativado com sucesso - CorrelationId: "+ masterCard.getCorrelationId())
 						.payload(masterCard).build());
 			}
+			
 		} catch (Exception e) {
 			// Trata-se de um erro recuperável?
 			final boolean recuperavel = isRecuperavel(e);
   
 			// Caso erro não seja recuperavel
 			if (!recuperavel) {
+				
 				LogSplunk.error(Splunk.builder().key("token.ativacao.erro.dlq")
 						.customMessage(e.getMessage())
 						.payload(payload).exception(e).build());
-
-				
+								
 				// Cria msg de dlq
-				Map<String, String> deadLeatterMsg = new HashMap<String, String>();
+				Map<String, Object> deadLeatterMsg = new HashMap<String, Object>();
 				deadLeatterMsg.put("payload", payload);
 				deadLeatterMsg.put("customMsg", e.getMessage());
 				
 				// Envia para fila de DeadLeatter
-				kafkaTemplate.send("MASTER_CARD_TOKEN_ACTIVATION-DLQ",
+				kafkaTemplate.send(TopicEnum.DLQ.getTopicName(),
 						masterCard != null ? masterCard.getNumeroCartao() : payload, 
 								deadLeatterMsg).get();
-
+				
 			} else {
 				// Log Erro Recuperavel
 				LogSplunk.error(Splunk.builder().key("token.ativacao.erro.recuperavel")
 						.customMessage(e.getMessage())
 						.payload(payload).exception(e).build());
 				
-				// Pausa o kafka por 0, apenas para não mover o offset da msg atual e reprocessar msg
-				ack.nack(0);
+				// Decide o topico de retry ou dlq a ser enviado
+				this.decideRetryOrDlq(masterCard, e);
 				
-				System.out.println("TEMPO DECORRIDO - " + (System.currentTimeMillis() - startTime));
-				return;
 			}
+		} finally {
+			// Commita leitura move o offSet
+			ack.acknowledge();
 		}
-
-		// Commita leitura move o offSet
-		ack.acknowledge();
-
+		
 		System.out.println("TEMPO DECORRIDO - " + (System.currentTimeMillis() - startTime));
 	}
 	
+	private void decideRetryOrDlq(MasterCardDTO masterCard, Exception e) throws InterruptedException, ExecutionException, JsonMappingException, JsonProcessingException {
+		// Atribui o numero da proxima
+		masterCard.setNumeroTentativa(masterCard.getNumeroTentativa() + 1);
+		
+		// Recupera o topico destino e o tempo de espera
+		TopicEnum topicoDestinoInfo = TopicEnum.getRetryTopicFromAttempt(masterCard.getNumeroTentativa());
+		
+		// Calcula o tempo de espera para a proxima execucao, adiciona os segundos de espera
+		masterCard.setDataHoraProximaTentativa(LocalDateTime.now().plusSeconds(topicoDestinoInfo.getSecondsAwait()));
+		
+		Map<String, Object> retryOrDLQrMsg = new HashMap<String, Object>();
+		retryOrDLQrMsg.put("payload", masterCard);
+		retryOrDLQrMsg.put("customMsg", e.getMessage());
+		
+		
+		/*JsonNode jsonNode = mapper.readTree(mapper.valueToTree(retryOrDLQrMsg));*/
+		
+		// Monta informações do topico e destino
+		Message<Map<String, Object>> record = MessageBuilder.
+	             withPayload(retryOrDLQrMsg).
+	             setHeader(KafkaHeaders.MESSAGE_KEY, masterCard.getNumeroCartao()).
+	             setHeader(KafkaHeaders.TOPIC, topicoDestinoInfo.getTopicName()).
+//	             setHeader(KafkaHeaders.TIMESTAMP, Instant.now().toEpochMilli()).
+//	             setHeader("dataHoraProximaTentativa", masterCard.getDataHoraProximaTentativa().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()).
+	             build();
+		
+		/*if(!topicoDestinoInfo.equals(TopicEnum.DLQ)) {
+			record.getHeaders().put("dataHoraProximaTentativa", masterCard.getDataHoraProximaTentativa().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+		}*/
+		
+		// Envia para o topico de destino
+		kafkaTemplate.send(record).get();
+		
+		//kafkaTemplate.send(topicoDestinoInfo.getTopicName(), masterCard.getNumeroCartao(), retryOrDLQrMsg).get();
+	}
+
+//	private void decideRetryOrDlq(ConsumerRecord<String, ?> consumerRecord) {
+//		// Envia para fila de DeadLeatter
+//		try{
+//			String payload = (String) consumerRecord.value().toString();
+//			kafkaTemplateRetry.send("MASTER_CARD_TOKEN_ACTIVATION-RETRY-1", consumerRecord.key(), payload);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+//	@RetryableTopic(attempts = "2", backoff = @Backoff(delay = 10000, multiplier = 2.0), autoCreateTopics = "true",
+//			topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
+//	@KafkaListener(topics = "MASTER_CARD_TOKEN_ACTIVATION-RETRY-1", groupId = "consumer_group1")
+//	public void listen(ConsumerRecord<String, ?> consumerRecord, Acknowledgment ack) {
+//		System.out.println("**** RETRY ****");
+//		System.out.println(consumerRecord.topic());
+//		System.out.println(consumerRecord.value());
+//		
+//		MasterCardDTO masterCard = null;
+//		String payload = (String) consumerRecord.value().toString();
+//		
+//		//char[] escaped = JsonStringEncoder.getInstance().quoteAsString(payload);
+//		String escaped = JSONValue.escape(payload);
+//		// Tenta transformar o payload em Objeto
+//		try {
+//			
+//			masterCard = mapper.readValue(escaped.toString(), MasterCardDTO.class);
+//		} catch (JsonMappingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (JsonProcessingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//			
+//		if(masterCard.getIdCartao().equals("e6271375-0471-450b-803a-d0a22b937572")) {
+//			ack.acknowledge(); 
+//		}
+//
+//		
+//		
+//		//log.info(consumerRecord.value() + " from " + topic);
+//		throw new RuntimeException("test");
+//	}
 	
 	
+
 	private boolean isEventoPermitido(MasterCardDTO masterCard) {
 		boolean carteiraPermitida = false;
 		
@@ -198,6 +349,7 @@ public class MasterCardConsumer {
 						// Para o listener do kafka
 						this.stopKafkaListener();
 						isRecuperavel = true;
+						return isRecuperavel;
 					}
 					
 				} else {
